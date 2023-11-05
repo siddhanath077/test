dfad27172b2939f6177176d3e9b4a422ba125816
tiwari <siddhanath077@gmail.com>
2023-11-05 10:30:49 +0530

    first set up commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    tis function will retun the list of requirements
+    '''
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements = [req.replace('\n',"") for req in requirements]
+    return requirements
+
+setup(
+    name='test',
+    version='0.0.1',
+    author='Siddhanath',
+    author_email='siddhanath077@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
